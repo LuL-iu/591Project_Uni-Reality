7019b743467536c85419aebff3e6cdf8ac93932d
Yixuan Chen <chenyc@seas.upenn.edu>
2019-07-25 23:30:02 -0400

    Javadoc added to Text

--- src/Text.java
+++ src/Text.java
@@ -2,18 +2,40 @@
 public class Text {
 	
 	
+	/**
+	 * This method takes keyboard input 
+	 * 
+	 * 
+	 *
+	 **/
 	public void typeText() {
 		
 		
-	}
-	
-	
-	public void selectFont() {
-		
 		
 	}
 	
-	public void selectColor() {
+	
+	/**
+	 * This method format Text into Font required  
+	 * 
+	 * 
+	 *
+	 **/
+	public void selectFont(String FontName) {
+		
+		
+		
+		
+	}
+
+	/**
+	 * This method format Text into different color based off RGB  
+	 * 
+	 * 
+	 *
+	 **/
+	public void selectColor(int R, int G, int B) {
+		
 		
 		
 	}
